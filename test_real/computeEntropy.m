490d4ca71fd567dff38ccc425a68e722f67275e5
cjs2094 <49527223+cjs2094@users.noreply.github.com>
2022-11-15 12:46:51 -0500

    Delete test_real directory

--- test_real/computeEntropy.m
+++
@@ -1,8 +0,0 @@
-function ret_entropy = computeEntropy(x, support)
-%% Chau-Wai Wong, Oct. 2016
-
-myEntropy = @(pmf) -pmf(:)'/sum(pmf) * log2(pmf(:)/sum(pmf));
-
-cnt_raw = hist(x, support);
-pmf_raw = cnt_raw/sum(cnt_raw);
-ret_entropy = myEntropy(pmf_raw(pmf_raw>0));
