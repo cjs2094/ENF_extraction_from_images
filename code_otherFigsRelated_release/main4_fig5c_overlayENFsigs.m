ad4aa2c113e531f57759bec5165c5f9bae1c0eac
cjs2094 <49527223+cjs2094@users.noreply.github.com>
2022-11-21 00:16:06 -0500

    Delete code_otherFigsRelated_release directory

--- code_otherFigsRelated_release/main4_fig5c_overlayENFsigs.m
+++
@@ -1,8 +0,0 @@
-clear all; close all; clc;
-
-%% show fig. 5.(c) of the paper - overlay averaged and detrended averaged column signals
-
-img_db_path = '.\imgs_set\';
-K = 200; % number of signals to be overlayed
-
-func_overlayAveragedAndDetrendedAveragedColSigs(img_db_path, K);
