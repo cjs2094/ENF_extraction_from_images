caa9eba72c5c36c30c2e1220acd7b35bb211c676
cjs2094 <49527223+cjs2094@users.noreply.github.com>
2022-11-20 22:31:34 -0500

    Delete code_forTestingSyntheticImgs_release directory

--- code_forTestingSyntheticImgs_release/func_subplottight2.m
+++
@@ -1,7 +0,0 @@
-function h = func_subplottight2(n,m,i)
-[c,r] = ind2sub([m n], i);
-%ax = subplot('Position', [(c-1)/m, 1-(r)/n - 0.09, 1/m, 1/n]);
-ax = subplot('Position', [(c-1)/m + 0.03, 1-(r)/n, 1/m, 1/n]);
-if(nargout > 0)
-    h = ax;
-end
