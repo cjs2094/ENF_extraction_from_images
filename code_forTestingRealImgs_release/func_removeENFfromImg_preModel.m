0f47cfee61c81184c6695e98b6e23a2bf91e4fd6
cjs2094 <49527223+cjs2094@users.noreply.github.com>
2022-11-21 00:20:26 -0500

    Add files via upload

---
+++ code_forTestingRealImgs_release/func_removeENFfromImg_preModel.m
@@ -0,0 +1,53 @@
+function [originalImgEstimate, sinusoidalPartEnf_entropy] = func_removeENFfromImg_preModel(img, imgSize, T_row, frequency, m_s, m_t, a, phi)
+%% 
+%  Jisoo Choi, 07/19/2019
+
+mRange = 1:imgSize(1);
+mRange = mRange(:);
+M = mRange(end);
+
+mag = a;
+
+sinusoidalPartEnf_entropy = mag .* cos(2*pi*frequency*T_row*mRange + phi);
+
+numOfTotalCols = imgSize(2);
+
+originalImgEstimate = img - sinusoidalPartEnf_entropy * ones(1, numOfTotalCols);
+
+clipLogSet1 = originalImgEstimate > 255;
+clipLogSet2 = originalImgEstimate < 0;
+clipPerc = mean(clipLogSet1(:)) + mean(clipLogSet2(:));
+disp(sprintf('clipped perc = %.1f%%', clipPerc*100));
+
+originalImgEstimate = uint8(originalImgEstimate);
+% originalImgEstimate = uint8(255*normalize(originalImgEstimate, 'range', [0 1]));
+
+sinusoidalPartEnf_entropy = sinusoidalPartEnf_entropy(m_s:m_t);
+
+disp(sprintf('\n'))
+
+% function [originalImgEstimate, enfEst] = func_removeENFfromImg(img, imgSize, T_row, a1_l, a1_r, a2, b2, c2, frequency, phi)
+% %% 
+% %  Jisoo Choi, 07/19/2019
+% 
+% yRange = [0 : (imgSize(1)-1)]';
+% 
+% scaling = ((a1_r-a1_l)/(imgSize(1)-1)*yRange + a1_l);
+% 
+% bias = a2*yRange.^2 + b2*yRange + c2;
+% 
+% enfEst = scaling .* cos(2*pi*frequency*T_row*yRange + phi) + bias;
+% 
+% numOfTotalCol = imgSize(2);
+% 
+% originalImgEstimate = img - enfEst * ones(1, numOfTotalCol);
+% 
+% clipLogSet = originalImgEstimate > 255;
+% clipPerc = mean(clipLogSet(:));
+% disp(sprintf('clipped perc = %.1f%%', clipPerc*100));
+% 
+% originalImgEstimate = uint8(originalImgEstimate);
+% % originalImgEstimate = uint8(255*normalize(originalImgEstimate, 'range', [0 1]));
+% 
+% disp(sprintf('\n'))
+
